1bbcde6dd430af63fdf353d8e2cf418b8bf20003
Alexander <AlexanderRy@protonmail.com>
2022-10-08 12:44:33 +0300

    Starting files

---
+++ src/main/java/net/me/leaveslab/leaveslabClient.java
@@ -0,0 +1,30 @@
+package net.me.leaveslab;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import net.minecraft.client.render.RenderLayer;
+
+public class leaveslabClient implements ClientModInitializer {
+
+
+
+
+    @Override
+    public void onInitializeClient() {
+        BlockRenderLayerMap.INSTANCE.putBlock(leaveslab.FLOWERING_AZALEA_LEAVES_SLAB, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(leaveslab.AZALEA_LEAVES_SLAB, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(leaveslab.ACACIA_LEAVES_SLAB, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(leaveslab.BIRCH_LEAVES_SLAB, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(leaveslab.DARK_OAK_LEAVES_SLAB, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(leaveslab.JUNGLE_LEAVES_SLAB, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(leaveslab.MANGROVE_LEAVES_SLAB, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(leaveslab.OAK_LEAVES_SLAB, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(leaveslab.SPRUCE_LEAVES_SLAB, RenderLayer.getCutout());
+
+
+
+
+
+    }
+}
